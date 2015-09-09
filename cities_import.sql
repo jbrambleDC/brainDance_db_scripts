07efcbc25a5c27a253f147e2d63502664638f4d1
jordan-bramble <jordan.bramble@opower.com>
2015-09-08 22:57:16 -0400

    first commit

---
+++ cities_import.sql
@@ -0,0 +1,12 @@
+drop table if exists tt_cities;
+CREATE TABLE tt_cities(
+  country_code varchar(2),
+  city varchar(40),
+  region varchar(5),
+  latitude float(10),
+  longitude float(10)
+);
+load data local infile '~/worldcitiespop.txt' into table tt_cities
+(country_code, city, @dummy1, region, @dummy2, latitude, longitude)
+fields terminated by ','
+ignore 1 lines;
